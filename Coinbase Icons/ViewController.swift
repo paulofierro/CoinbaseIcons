920c1ff32d094bbea3edcfc707b189ae4d2e784c
Paulo Fierro <paulo@paulofierro.com>
2019-04-30 23:08:25 -0500

    Initial Commit

---
+++ Coinbase Icons/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coinbase Icons
+//
+//  Created by Paulo Fierro on 30/04/2019.
+//  Copyright © 2019 Jadehopper Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
